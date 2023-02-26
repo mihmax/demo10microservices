2d9b0e22efca58aaceb43101412e8f7adafe6325
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-26 16:19:17 +0200

    Bugfix -- need a default constructor for events

--- account-common/src/main/java/ua/dp/maxym/account/common/events/AccountClosedEvent.java
+++ account-common/src/main/java/ua/dp/maxym/account/common/events/AccountClosedEvent.java
@@ -2,6 +2,7 @@ package ua.dp.maxym.account.common.events;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import ua.dp.maxym.cqrs.core.events.BaseEvent;
 
@@ -9,5 +10,6 @@ import ua.dp.maxym.cqrs.core.events.BaseEvent;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder
+@NoArgsConstructor
 public class AccountClosedEvent extends BaseEvent {
 }
