32618f236a74cf799c1e24c5595a8380e0994b55
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-26 13:08:54 +0200

    Event sourcing handler+

--- cqrs-core/src/main/java/ua/dp/maxym/cqrs/core/handlers/EventSourcingHandler.java
+++ cqrs-core/src/main/java/ua/dp/maxym/cqrs/core/handlers/EventSourcingHandler.java
@@ -2,7 +2,7 @@ package ua.dp.maxym.cqrs.core.handlers;
 
 import ua.dp.maxym.cqrs.core.domain.AggregateRoot;
 
-public interface EventSourcingHandler<T> {
+public interface EventSourcingHandler<T extends AggregateRoot> {
     void save(T aggregate);
     T getById(String aggregateId);
 }
