32275438a9d4439290f009e949b6a426dda70141
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-26 13:11:31 +0200

    Command handler registration

--- account-cmd/src/main/java/ua/dp/maxym/account/cmd/AccountCommandApplication.java
+++ account-cmd/src/main/java/ua/dp/maxym/account/cmd/AccountCommandApplication.java
@@ -1,10 +1,30 @@
 package ua.dp.maxym.account.cmd;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ua.dp.maxym.account.cmd.api.commands.*;
+import ua.dp.maxym.cqrs.core.infrastructure.CommandDispatcher;
 
 @SpringBootApplication
 public class AccountCommandApplication {
+
+    private final CommandDispatcher commandDispatcher;
+    private final CommandHandler commandHandler;
+
+    public AccountCommandApplication(CommandDispatcher commandDispatcher, CommandHandler commandHandler) {
+        this.commandDispatcher = commandDispatcher;
+        this.commandHandler = commandHandler;
+    }
+
+    @PostConstruct
+    public void registerHandlers() {
+        commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountCommandApplication.class, args);
