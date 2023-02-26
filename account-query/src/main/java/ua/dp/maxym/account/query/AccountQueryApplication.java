135990d8dbbe12f750a887b7f26b9da9abe836a6
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-26 18:00:01 +0200

    Query handler registration

--- account-query/src/main/java/ua/dp/maxym/account/query/AccountQueryApplication.java
+++ account-query/src/main/java/ua/dp/maxym/account/query/AccountQueryApplication.java
@@ -1,10 +1,28 @@
 package ua.dp.maxym.account.query;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ua.dp.maxym.account.query.api.queries.*;
+import ua.dp.maxym.cqrs.core.infrastructure.QueryDispatcher;
 
 @SpringBootApplication
 public class AccountQueryApplication {
+    private final QueryDispatcher queryDispatcher;
+    private final QueryHandler queryHandler;
+
+    public AccountQueryApplication(QueryDispatcher queryDispatcher, QueryHandler queryHandler) {
+        this.queryDispatcher = queryDispatcher;
+        this.queryHandler = queryHandler;
+    }
+
+    @PostConstruct
+    public void registerHandlers() {
+        queryDispatcher.registerHandler(FindAllAccountsQuery.class, queryHandler::handle);
+        queryDispatcher.registerHandler(FindAccountsByIdQuery.class, queryHandler::handle);
+        queryDispatcher.registerHandler(FindAccountsByHolderQuery.class, queryHandler::handle);
+        queryDispatcher.registerHandler(FindAccountsWithBalanceQuery.class, queryHandler::handle);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountQueryApplication.class, args);
